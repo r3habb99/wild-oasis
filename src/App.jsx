3c494f0be816992e1652e75dfc9464e3f1fdab83
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-05-15 13:19:05 +0530

    Added react-router into project

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,66 @@
-import styled from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
 
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+//Styles
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The World Wise</Heading>
-            <div>
-              <Heading as="h2">Checked In and Out</Heading>
-              <Button onClick={() => alert('You Checked In')}>Check In</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert('You Check Out')}
-              >
-                Check Out
-              </Button>
-            </div>
-          </Row>
-          <Row type="vertical">
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input
-                type="number"
-                placeholder="Number of Guest"
+      <BrowserRouter>
+        <Routes>
+          <Route
+            index
+            element={
+              <Navigate
+                replace
+                to="dashboard"
               />
-              <Input
-                type="number"
-                placeholder="Number of Guest"
-              />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+            }
+          />
+          <Route
+            path="dashboard"
+            element={<Dashboard />}
+          />
+          <Route
+            path="bookings"
+            element={<Bookings />}
+          />
+          <Route
+            path="cabins"
+            element={<Cabins />}
+          />
+          <Route
+            path="users"
+            element={<Users />}
+          />
+          <Route
+            path="settings"
+            element={<Settings />}
+          />
+          <Route
+            path="account"
+            element={<Account />}
+          />
+
+          <Route
+            path="login"
+            element={<Login />}
+          />
+          <Route
+            path="*"
+            element={<PageNotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
