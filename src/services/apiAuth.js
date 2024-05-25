b0cce89c21966a9307a443a21e480bb716881bdd
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-05-25 11:09:34 +0530

    Added update user profile and user password functions

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -42,4 +42,38 @@ export async function logout() {
   if (error) {
     throw new Error(`Get Current User Failed to Fetch ${error.message}`);
   }
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+  // 1. Update password or fullName
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) {
+    throw new Error(`Current user failed to update ${error.message}`);
+  }
+  if (!avatar) return data;
+  // 2. Upload the avatar image
+  const fileName = `avatar=${data.user.id}-${new Date().toISOString()}`;
+  const { error: storageError } = await supabase.storage
+    .from('avatars')
+    .upload(fileName, avatar);
+  if (storageError) {
+    throw new Error(
+      `Current user avatar upload failed ${storageError.message}`
+    );
+  }
+  // 3. Upload avatar in the user
+  const { data: updatedUser, error: updatedError } = supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+  if (updatedError) {
+    throw new Error(
+      `Current user updation failed with avatar ${updatedError.message}`
+    );
+  }
+  return updatedUser;
 }
