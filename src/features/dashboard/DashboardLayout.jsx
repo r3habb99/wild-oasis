7afabd0fc3aa5425df9df2fe68f03810343b8129
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-05-25 16:00:21 +0530

    Added Pie chart using rechart library to show confirmedstays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import { useRecentStays } from './useRecentStays-hook';
 import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins-hook';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -35,6 +36,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart
         bookings={bookings}
         numDays={numDays}
